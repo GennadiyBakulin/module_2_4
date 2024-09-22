4a50da659d6ccb92d224dcf8f30c6f0958b738ae
Gennadiy Bakulin <g.bakulin@yandex.ru>
2024-09-22 13:18:46 +0700

    create ex3 homework4

--- src/org/javaacademy/homework/homework4/ex3/Runner.java
+++ src/org/javaacademy/homework/homework4/ex3/Runner.java
@@ -1,5 +1,7 @@
 package org.javaacademy.homework.homework4.ex3;
 
+import java.util.List;
+
 public class Runner {
     public static void main(String[] args) {
         //Задание №3 - Небоскребы, небоскребы, а я...
@@ -12,7 +14,17 @@ public class Runner {
         //Абрадж аль-Бейт - 601м
         //Всемирный центр Лотте - 555м
 
+        Skyscraper worldTradeCenter = new Skyscraper("World Trade Center", 541);
+        Skyscraper shanghaiTower = new Skyscraper("Shanghai Tower", 632);
+        Skyscraper burjKhalifa = new Skyscraper("Burj Khalifa", 828);
+        Skyscraper pinanInternationalFinancialCenter = new Skyscraper("Pinan International Financial Center", 599);
+        Skyscraper abrajAlBayt = new Skyscraper("Abraj al-Bayt", 601);
+        Skyscraper lotteWorldCenter = new Skyscraper("Lotte World Center", 555);
+
         //3. Занести небоскребы в List! Дважды положить бурдж халифа в лист.
+
+        List<Skyscraper> skyscraperList = List.of(worldTradeCenter, shanghaiTower, burjKhalifa,
+                pinanInternationalFinancialCenter, abrajAlBayt, lotteWorldCenter, burjKhalifa);
 
         //4. С помощью стримов:
         //4.1 Убрать дубликаты и сохранить в новый лист (далее работаем с этим листом).
@@ -21,5 +33,39 @@ public class Runner {
         //4.3. Вывести самый высокий небоскреб.
         //4.4. Вывести те небоскребы, которые выше километра.
         //Если выше километра нет, то вывести на экран: небоскреба выше километра - нет.
+
+        List<Skyscraper> newSkyscraperList = skyscraperList.stream()
+                .distinct()
+                .toList();
+
+        newSkyscraperList.forEach(System.out::println);
+
+        System.out.println("-----------------------------------");
+        System.out.println("Первые три небоскреба:");
+
+        newSkyscraperList.stream()
+                .limit(3)
+                .forEach(System.out::println);
+
+        System.out.println("-----------------------------------");
+        System.out.println("Самый высокий небоскреб:");
+
+        newSkyscraperList.stream()
+                .sorted((o1, o2) -> o2.getHeight() - o1.getHeight())
+                .limit(1)
+                .forEach(System.out::println);
+
+        System.out.println("-----------------------------------");
+        System.out.println("Вывести те небоскребы, которые выше километра:");
+
+        int maxHeight = 1000;
+
+        if (newSkyscraperList.stream().noneMatch(skyscraper -> skyscraper.getHeight() > maxHeight)) {
+            System.out.println("небоскреба выше километра - нет");
+        } else {
+            newSkyscraperList.stream()
+                    .filter(skyscraper -> skyscraper.getHeight() > maxHeight)
+                    .forEach(System.out::println);
+        }
     }
 }
